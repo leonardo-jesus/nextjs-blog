092d055be57a501f8aa1b92e7b47442c8ed34904
leonardo-jesus <leonardojesus02@outlook.com>
2021-10-25 19:21:19 -0300

    feat(incrementalPage): Add page structure

--- pages/incremental.js
+++ pages/incremental.js
@@ -1,17 +1,31 @@
-import { getBFFJson } from '../helpers/postsHelper'
+import utilStyles from '../styles/utils.module.css';
 
 export default function Post({ projects }) {
   return (
-    projects
-  )
+    <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+      <h2 className={utilStyles.headingLg}>My projects</h2>
+      <ul className={utilStyles.list}>
+        {projects.map(({ title, description }) => (
+          <li className={utilStyles.listItem} key={title}>
+            <h2 className={utilStyles.headingLg}>{title}</h2>
+          <br />
+          <small className={utilStyles.lightText}>
+            <p>{description}</p>
+          </small>
+        </li>
+        ))}
+      </ul>
+    </section>
+  );
 }
 
 export async function getStaticProps() {
-  const projects = await getBFFJson()
+  const projects = await fetch('http://localhost:3333/projects');
+  const projectsJSON = await projects.json();
   return {
     props: {
-      projects
+      projects: projectsJSON
     },
-    revalidate: 15,
-  }
+    revalidate: 5,
+  };
 }
